0a96ee948f31d551f3cfcc3ae93f846f7df488bf
Md Sahil <152709288+Mdsahil134@users.noreply.github.com>
2026-07-08 11:48:36 +0530

    Time: 19 ms (85.52%), Space: 23.4 MB (91.56%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -10,30 +10,16 @@
 
 class Solution {
 public:
-    void findPath(TreeNode* root, TreeNode* p,vector<TreeNode*>& path){
-        if(root==NULL)return;
-        path.push_back(root);
-         if (root == p) return;
-        if(root->val > p->val){
-            findPath(root->left,p,path);
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        
+        if(root!=NULL && root->val > p->val && root->val > q->val){
+            return lowestCommonAncestor(root->left,p,q);
+        }
+        else if(root!=NULL && root->val < p->val && root->val < q->val){
+            return lowestCommonAncestor(root->right,p,q);
         }
         else{
-             findPath(root->right,p,path);
+            return root;
         }
-    }
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root==NULL)return NULL;
-        vector<TreeNode*> p_path;
-        vector<TreeNode*> q_path;
-        findPath(root,p,p_path);
-        findPath(root,q,q_path);
-        int n=min(p_path.size(),q_path.size());
-        
-        for(int i=0;i<n;i++){
-            if(p_path[i] != q_path[i]){
-                return p_path[i-1];
-            }
-        }
-        return (p_path.size()< q_path.size()?p_path.back():q_path.back());
     }
 };
